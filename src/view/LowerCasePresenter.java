b9e36f82c48e2c955dbb84d561cbc03a5ebf2072
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-02-14 20:52:53 +0100

    Integreret Anders' løsning til JavaFX delen.

--- src/view/LowerCasePresenter.java
+++ src/view/LowerCasePresenter.java
@@ -6,17 +6,36 @@ import model.Model;
 import java.util.Observable;
 import java.util.Observer;
 
-public class LowerCasePresenter implements Observer {
+public class LowerCasePresenter extends Observable {
     private String string;
+    private Model model;
 
-    @Override
-    public void update(Observable o, Object arg) {
-        if (o instanceof Model) {
-            string = ((Model) o).getInput().toLowerCase();
-        }
+    public LowerCasePresenter(Model model) {
+        this.model = model;
+        observeModel(this.model);
+        string = this.model.getInput().toLowerCase();
+    }
+
+    private void observeModel(Model model) {
+        model.addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                if (o instanceof Model) {
+                    String data = ((Model) o).getInput();
+                    string = data.toLowerCase();
+
+                    LowerCasePresenter.super.setChanged();
+                    LowerCasePresenter.super.notifyObservers();
+                }
+            }
+        });
     }
 
     String getString() {
         return string;
+    }
+
+    public void setString(String string) {
+        model.setInput(string);
     }
 }
